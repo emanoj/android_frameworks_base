d726b210c1cc6bcc70ecc73fd6ffa876c54a7a70
Sam Mortimer <sam@mortimer.me.uk>
2014-11-16 20:03:53 -0800

    LED lights customization [1/3]
    [1/2] frameworks/base: Battery and Notification Lights
    
    PS2: *) Add back a few missing pieces
         *) Do not clear light state in onPanelRevealed(),
            update lights on ACTION_SCREEN_ON instead
    
    PS3: *) Wrap long lines
         *) Disable config_multiColorNotificationLed by default
         *) Revert config_defaultNotificationLedOff
            from 1000 back to AOSP default of 2000
    
    PS4: *) Clear lights in onPanelRevealed() if
            keyguard is not active.
    
    PS5: Coding style tuning
    
    Pulled together from:
    
    Author: DvTonder <david.vantonder@gmail.com>
    Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Date:   Mon Nov 4 00:57:51 2013 +0000
    Framework: Port CM9 features to CM10
    Change-Id: Ibd63116df90b06f6ce6adb8a0343059bbb999bfb
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Sun Dec 8 15:24:41 2013 +0700
    BatteryService: fix FC on boot until battery stat is present
    updateLightsLocked() can be called from CM's added SettingsObserver
    when battery stat is not present, causing an FC and a loop until
    battery stat is present.
    Change-Id: Ic4438fe50e98f1aa05ae1d0d26240bf9410fd92f
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Tue Dec 31 16:22:05 2013 -0800
    [2/2] Framework: instant led test
    Adds support a new notification extra boolean
    EXTRA_FORCE_SHOW_LIGHTS.  Used by settings
    notification light picker to force lights on
    when the screen is on.
    Change-Id: If0a42d32b28fe8c02ef5f7dd148db7eb478fac17
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Wed Apr 24 11:09:55 2013 +0200
    Allow creating custom dialogs in DialogPreference.
    This allows creating non-AlertDialogs while still using the click and
    state handling of DialogPreference.
    Change-Id: I54fd7d6d6b9f6a49fef1ae95e0178838b3edfa56
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Mon Aug 18 04:56:28 2014 +0300
    Add support for single color notification LED (1/2)
    Change-Id: I367af77036da9e87c6dd0df552ce4c56d945a44d
    
    Lights with Screen On [1/2]: Optional allowment of lights
    Implement a setting allowing lights to be activated
    for new notifications even if the screen is on.
    
    Lights with screen on and Custom values are separated
    in an advanced section for a cleaner overview.
    This setting gives the user an oportunity to activate lights
    with the screen on and also during DayDream screensaver.
    The option is not activated by default.
    
    Screenshot of the Settings :
      http://i1285.photobucket.com/albums/a583/adriandc/Screenshot_2015-07-20-23-47-13_zpstpmemwwn.png~original
    
    Lights with screen on: Don't disable leds after the lockscreen
    - If lights with screen on is enabled, do not disable leds
      when user passes through the lockscreen.
    
    framework: add config_ledCanPulse overlay setting
    Some devices do not support pulsing LEDs. Allow them to hide pulse
    settings.
    
    FWB: Battery light: 100% charged level (1/2)
    
    Frameworks: Allow/Prevent notification light in Zen mode (1 of 2)
    This allows the user to prevent the notification lights from showing during Zen mode
    
    lights: Make sure the lights are out
    
     * Annoying situation where the battery charging LED comes on in
       the crypto startup screen, but never gets turned off because
       of caching.
     * Explicitly turn off LEDs after the bootanimation completes, and
       always set the full state to the hardware on the first call
       regardless if it's all zeros.
    
    Change-Id: I8cd2325ec5d8924391e4fc62249f6dd737d52f3e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3548,6 +3548,50 @@ public final class Settings {
          */
 
         /**
+         * Whether the notification light will be allowed when in zen mode during downtime
+         * @hide
+         */
+        public static final String ALLOW_LIGHTS = "allow_lights";
+
+        /**
+         * What color to use for the battery LED while charging - low
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_LOW_COLOR = "battery_light_low_color";
+
+        /**
+         * What color to use for the battery LED while charging - medium
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_MEDIUM_COLOR = "battery_light_medium_color";
+
+        /**
+         * What color to use for the battery LED while charging - full
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_FULL_COLOR = "battery_light_full_color";
+
+        /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
+        /**
+         * Whether the battery light should be enabled (if hardware supports it)
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
+
+        /**
+         * Whether the battery LED should repeatedly flash when the battery is low
+         * on charge. The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_PULSE = "battery_light_pulse";
+
+        /**
          * The OpenCNAM paid account ID
          * @hide
          */
@@ -3719,6 +3763,49 @@ public final class Settings {
          * @hide
          */
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
+         * What color to use for the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR =
+                "notification_light_pulse_default_color";
+
+        /**
+         * How long to flash the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON =
+                "notification_light_pulse_default_led_on";
+
+        /**
+         * How long to wait between flashes for the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF =
+                "notification_light_pulse_default_led_off";
+
+        /**
+         * Whether to use the custom LED values for the notification pulse LED.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE =
+                "notification_light_pulse_custom_enable";
+
+        /**
+         * Which custom LED values to use for the notification pulse LED.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES =
+                "notification_light_pulse_custom_values";
+
+        /**
+         * Whether to allow notifications with the screen on or DayDreams.
+         * The value is boolean (1 or 0). Default will always be false.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_SCREEN_ON =
+                "notification_light_screen_on_enable";
 
         /**
          * Whether the proximity sensor will adjust call to speaker
@@ -3930,6 +4017,8 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
+            NOTIFICATION_LIGHT_SCREEN_ON,
+            ALLOW_LIGHTS,
             INCREASING_RING,
             INCREASING_RING_START_VOLUME,
             INCREASING_RING_RAMP_UP_TIME
